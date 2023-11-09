ac1243d258de75a9fbf21d517a6f296a8fe3bbbe
Iulius Curt <iulius.curt@gmail.com>
2023-11-09 11:39:26 +0200

    Update layout

--- components/layout.js
+++ components/layout.js
@@ -3,18 +3,34 @@ import Link from 'next/link';
 export default function Layout({ children }) {
   return (
     <div className='flex flex-col min-h-screen'>
-      <header className='bg-fuchsia-100 mb-8 py-4'>
-        <div className='container mx-auto flex justify-center'>
-          <Link href='/'>
-            <a>🏡</a>
-          </Link>
-          <span className='mx-auto'>Welcome to my blog</span>{' '}
+      <header className='goldbg mb-8 py-4'>
+        <div className='container mx-auto flex'>
+          <a href='https://nutasiasociatii.ro/'>
+            <img src="/logo.svg" className="logo" alt="Nuță și Asociații"/>
+          </a>
+          <div className="menu">
+            <a href="https://nutasiasociatii.ro/#about">Despre</a>
+            <a href="https://nutasiasociatii.ro/#services">Servicii juridice</a>
+            <a href="https://nutasiasociatii.ro/#practice-areas">Arii de practică</a>
+            <a href="/">Blog</a>
+            <a href="https://nutasiasociatii.ro/#contact">Contact</a>
+          </div>
         </div>
       </header>
       <main className='container mx-auto flex-1'>{children}</main>
-      <footer className='bg-fuchsia-100 mt-8 py-4'>
+      <footer className='goldbg mt-8 py-4'>
+        <div className='container mx-auto flex justify-space-between'>
+          <span>Alexandru Nuță &copy; All rights reserved</span>
+          <div className='footerLinks'>
+            <a href='https://www.facebook.com/profile.php?id=100090135618439&mibextid=ZbWKwL'>
+            </a>
+            <a href='https://www.linkedin.com/in/alexandru-nu%C5%A3%C4%83-ba93811ab/?originalSubdomain=ro'>
+            </a>
+            <a href='tel:+40783109488'>
+            </a>
+          </div>
+        </div>
         <div className='container mx-auto flex justify-center'>
-          &copy; 2022 DailyDevTips
         </div>
       </footer>
     </div>
